25a262a1e259a5c1066a1d31eb2ec07adf4c6979
takeshi-hashimoto0607 <b8011079t@gmail.com>
2026-05-20 23:58:49 +0900

    テスト手法の違い

--- spec/is_even_spec.rb
+++ spec/is_even_spec.rb
@@ -4,9 +4,13 @@ require_relative '../is_even'
 RSpec.describe 'is_even' do
   it 'returns true for even numbers' do
     expect(is_even(4)).to eq(true)
+    expect(is_even(0)).to eq(true)
+    expect(is_even(-2)).to eq(true)
+
   end
 
   it 'returns false for odd numbers' do
     expect(is_even(5)).to eq(false)
+    expect(is_even(101)).to eq(false)
   end
 end
